e80b596afb1ba3f802537950a45086c28b720df3
Alexey Sobolev <sobolev@mail.de>
2018-12-07 18:16:57 +0100

    Updated MAM.fetch functionality

--- src/utils/MAM.js
+++ src/utils/MAM.js
@@ -1,10 +1,11 @@
-import Mam from 'mam.client.js';
 import { trytesToAscii } from '@iota/converter'
+import { createHttpClient } from '@iota/http-client'
+import { createContext, Reader, Mode } from 'mam.client.js/lib/mam'
 
 export const fetch = (
   provider,
   root,
-  mode = 'public',
+  selectedMode = 'public',
   key = null,
   reportEvent,
   onFetchComplete
@@ -12,11 +13,22 @@ export const fetch = (
   if (!provider || !root) return;
   const promise = new Promise(async (resolve, reject) => {
     try {
-      Mam.init(provider);
-      await Mam.fetch(root, mode, key, data => {
-        const event = JSON.parse(trytesToAscii(data));
-        reportEvent(event);
-      });
+      const client = createHttpClient({ provider })
+      let ctx = await createContext()
+      let newRoot = root
+      let hasMessage
+      const mode = selectedMode === 'public' ? Mode.Public : Mode.Old
+
+      do {
+        let reader = new Reader(ctx, client, mode, newRoot, key);
+        const message = await reader.next();
+        hasMessage = message && message.value && message.value[0]
+        if (hasMessage) {
+          newRoot = message.value[0].message.nextRoot;
+          const event = JSON.parse(trytesToAscii(message.value[0].message.payload))
+          reportEvent(event)
+        }
+      } while(hasMessage)
       return resolve(onFetchComplete());
     } catch (error) {
       console.log('MAM fetch error', error);
