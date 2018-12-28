a7f782c9ba908ce1261b8b65c4c65f9ca8b2e677
Alexey Sobolev <sobolev@mail.de>
2018-12-28 17:02:39 +0100

    Updated MAM

--- src/utils/MAM.js
+++ src/utils/MAM.js
@@ -1,6 +1,7 @@
-import { trytesToAscii } from '@iota/converter'
-import { createHttpClient } from '@iota/http-client'
-import { createContext, Reader, Mode } from 'mam.client.js/lib/mam'
+import { trytesToAscii } from '@iota/converter';
+import { createHttpClient } from '@iota/http-client';
+import { createContext, Reader, Mode } from 'mam.client.js/lib/mam';
+import _get from 'lodash.get';
 
 export const fetch = (
   provider,
@@ -13,22 +14,22 @@ export const fetch = (
   if (!provider || !root) return;
   const promise = new Promise(async (resolve, reject) => {
     try {
-      const client = createHttpClient({ provider })
-      let ctx = await createContext()
-      let newRoot = root
-      let hasMessage
+      const client = createHttpClient({ provider });
+      let ctx = await createContext();
+      let newRoot = root;
+      let payload;
       const mode = selectedMode === 'public' ? Mode.Public : Mode.Old
 
       do {
         let reader = new Reader(ctx, client, mode, newRoot, key);
         const message = await reader.next();
-        hasMessage = message && message.value && message.value[0]
-        if (hasMessage) {
-          newRoot = message.value[0].message.nextRoot;
-          const event = JSON.parse(trytesToAscii(message.value[0].message.payload))
-          reportEvent(event)
+        payload = _get(message, 'value[0].message.payload');
+        newRoot = _get(message, 'value[0].message.nextRoot');
+        if (payload && newRoot) {
+          const event = JSON.parse(trytesToAscii(payload));
+          reportEvent(event);
         }
-      } while(hasMessage)
+      } while(payload);
       return resolve(onFetchComplete());
     } catch (error) {
       console.log('MAM fetch error', error);
