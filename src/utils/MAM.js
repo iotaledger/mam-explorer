897c578681885d7c7a66bc65d5af219ae8305583
Alexey Sobolev <sobolev@mail.de>
2019-02-08 15:35:27 +0100

    Refactored MAM call

--- src/utils/MAM.js
+++ src/utils/MAM.js
@@ -1,35 +1,13 @@
+import Mam from 'mam.client.js';
 import { trytesToAscii } from '@iota/converter';
-import { createHttpClient } from '@iota/http-client';
-import { createContext, Reader, Mode } from 'mam.client.js/lib/mam';
-import _get from 'lodash.get';
 
-export const fetch = (
-  provider,
-  root,
-  selectedMode = 'public',
-  key = null,
-  reportEvent,
-  onFetchComplete
-) => {
+export const fetch = (provider, root, mode, key, reportEvent, onFetchComplete) => {
   if (!provider || !root) return;
   const promise = new Promise(async (resolve, reject) => {
     try {
-      const client = createHttpClient({ provider });
-      let ctx = await createContext();
-      let newRoot = root;
-      let payload;
-      const mode = selectedMode === 'public' ? Mode.Public : Mode.Old
-
-      do {
-        let reader = new Reader(ctx, client, mode, newRoot, key);
-        const message = await reader.next();
-        payload = _get(message, 'value[0].message.payload');
-        newRoot = _get(message, 'value[0].message.nextRoot');
-        if (payload && newRoot) {
-          const event = JSON.parse(trytesToAscii(payload));
-          reportEvent(event);
-        }
-      } while(payload);
+      Mam.init(provider);
+      const convertAndReport = event => reportEvent(JSON.parse(trytesToAscii(event)))
+      await Mam.fetch(root, mode, key || '', convertAndReport);
       return resolve(onFetchComplete());
     } catch (error) {
       console.log('MAM fetch error', error);
