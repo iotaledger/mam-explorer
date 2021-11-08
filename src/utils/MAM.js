4bf1ad8352921b4877562d93fb5c4a5ce2f5eb48
Tim <tim.sigl@hotmail.de>
2021-11-08 09:43:10 +0100

    update mam to newer package

--- src/utils/MAM.js
+++ src/utils/MAM.js
@@ -1,14 +1,13 @@
-import Mam from 'mam.client.js';
+import { mamFetch } from '@iota/mam.js';
 import { trytesToAscii } from '@iota/converter';
 
-export const fetch = (provider, root, mode, key, reportEvent, onFetchComplete) => {
+export const fetch = (provider, root, mode, key) => {
   if (!provider || !root) return;
   const promise = new Promise(async (resolve, reject) => {
     try {
-      Mam.init(provider);
-      const convertAndReport = event => reportEvent(JSON.parse(trytesToAscii(event)))
-      await Mam.fetch(root, mode, key, convertAndReport);
-      return resolve(onFetchComplete());
+      const message = await mamFetch(provider, root, mode, key);
+      const decoded = decodeURIComponent(trytesToAscii(message.message)); 
+      return resolve(JSON.parse(decoded));
     } catch (error) {
       console.log('MAM fetch error', error);
       return reject();
